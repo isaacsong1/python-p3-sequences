093b059a7ed95742825fa7ff66e270c86961264e
Isaac Song <isaacsongs10@gmail.com>
2023-10-23 19:23:29 -0700

    My sequences lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibonacci_array = [0, 1]
+        if length > 2:
+            for i in range(length - 2):
+                fibonacci_array.append(fibonacci_array[i + 1] + fibonacci_array[i])
+        print(fibonacci_array)
